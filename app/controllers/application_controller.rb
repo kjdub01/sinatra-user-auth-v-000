c47001caba2ef9fb5c4cb657e007821cf72b9d34
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-12-24 03:01:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(email: params["email"], password: params["password"])
     session[:id] = @user.id
     
-    redirect '/users/home'
+    redirect '/sessions/login'
   end
 
   get '/sessions/logout' do 
